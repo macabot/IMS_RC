934d7020576b1eee5fc9ff4af36d92b8a69623fb
Michael Cabot <michael.a.cabot@gmail.com>
2012-12-06 11:29:12 +0100

    fixed brute force tracker

--- bruteForce.m
+++ bruteForce.m
@@ -6,29 +6,31 @@ files = dir(strcat(folder, '/*.png'));
 im1 = imread(strcat(folder, '/', files(1).name));
 % NOTE: pickSubimage/2 is in debugmode and chooses default subimage
 [target, centre] = pickSubimage(im1, 1); % pick target
-xCentre = centre(1);
-yCentre = centre(2);
-halfWidthScene = size(target,1);
-halfHeightScene = size(target,2);
+yCentre = centre(1);
+xCentre = centre(2);
+halfWidthScene = size(target,1); % scene-width is 2 times target-width
+halfHeightScene = size(target,2); % scene-height is 2 times target-height
 
-distType = 'batt';
+distType = 'bhatt'; % bhattacharyya distance
 for i=1:numel(files)
     figure(1);
     clf;
-    hold on;
     % read/show image
     im = imread(strcat(folder, '/', files(i).name));
     imshow(im);
+    hold on;
     title(files(i).name)
     % find target
-    scene = im(yCentre-halfWidthScene:yCentre+halfWidthScene, ...
-        xCentre-halfHeightScene:xCentre+halfHeightScene, :);
+    scene = im(xCentre-halfWidthScene:xCentre+halfWidthScene, ...
+        yCentre-halfHeightScene:yCentre+halfHeightScene, :);
     [xNew,yNew] = findTarget(scene, target, bins, distType);
     xCentre = xNew+xCentre-halfWidthScene;
     yCentre = yNew+yCentre-halfHeightScene;
-    % draw match    
-    upperLeft = [xCentre-size(target,2)/2, yCentre-size(target,1)/2];
+    % draw match 
+    plot(yCentre, xCentre, 'x', 'MarkerSize', 10, 'LineWidth', 3);
+    upperLeft = [yCentre-size(target,2)/2, xCentre-size(target,1)/2];
     rectangle('Position', [upperLeft(1), upperLeft(2),...
         size(target,2), size(target,1)])
 end
 hold off;
+title('Finished');
--- histDist.m
+++ histDist.m
@@ -2,9 +2,9 @@
 
 function dist = histDist(histA, histB, type)
 dist = -1;
-if strcmp(type, 'eucl')
+if strcmp(type, 'eucl') % euclidean distance
    dist = sqrt(sum(sum(sum( (histA - histB).^2 ))));
-elseif strcmp(type, 'batt')
+elseif strcmp(type, 'bhatt') % bhattacharyya distance
    coefficient = sum(sum(sum(sqrt(histA.*histB))));
    dist = sqrt(1-coefficient);
 end
