4a7f7033010d3953dc2658a960851dcf8752daf1
Michael Cabot <michael.a.cabot@gmail.com>
2012-12-06 20:13:06 +0100

    makeKernel makes epanechnikov kernel. Input to kernel, distances, is probably wrong.

---
+++ makeKernel.m
@@ -0,0 +1,12 @@
+% Richard Rozeboom (6173292) and Michael Cabot (6047262)
+
+function kernel = makeKernel(size, type, parameters)
+size = size-1;
+[X, Y] = meshgrid(-size(2)/2:size(2)/2, -size(1)/2:size(1)/2);
+distances = sqrt(X.^2+Y.^2);
+if strcmp(type, 'epan') % Epanechnikov kernel
+    c = parameters(1);
+    d = parameters(2);
+    kernel = (0.5/c)*(d+2)*(1-distances);
+    kernel(distances>1) = 0;
+end
