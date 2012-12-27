492b9dd2bcfbf35a9fef44e301f412fd0cc15b4d
Michael Cabot <michael.a.cabot@gmail.com>
2012-12-27 18:40:08 +0100

    add comments to readme and mean shift tracker

--- meanShiftTracker.m
+++ meanShiftTracker.m
@@ -1,4 +1,15 @@
 % Richard Rozeboom (6173292) and Michael Cabot (6047262)
+% Input:
+% - folder : directory of frames that contain target
+% - bins : amount of bins for the histogram. Note that there are actually bins^3 bins in the histogram.
+% - groundTruth : Matrix containing the true locations of the target for each frame.
+% - player : Name of the target.
+% - colorSpace : Color space that the target and frames should be converted to.
+% 
+% Output:
+% - frames : Struct containing images annotated with ground truth (green) and mean shift (blue) location.
+% - averageDuration : average duration of Mean Shift Tracker.
+% - error : average distance between ground truth and mean shift location.
 
 function [frames, averageDuration, error] = meanShiftTracker(folder, bins, ...
     groundTruth, player, colorSpace)
